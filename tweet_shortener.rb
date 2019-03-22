1bb6d3762b084e11d7aa7f840aa4de791fed077f
Kevin Malazarte <kevin.malazarte@gmail.com>
2019-03-22 00:38:15 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -45,7 +45,7 @@ end
 
 def shortened_tweet_truncator(tweet)
   if word_substituter(tweet).length > 140
-    "#{word_substituter(string)[0..136]..."
+    "#{word_substituter(string)[0..136]}..."
   else 
   word_substituter(string)
 end
